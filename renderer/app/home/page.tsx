d1e66581b6d703d505b1b8d218a263de90242fa8
Marcin Marciniuk <marcinm222@gmail.com>
2025-04-04 18:44:33 +0200

    improved codebase

--- renderer/app/home/page.tsx
+++ renderer/app/home/page.tsx
@@ -1,10 +1,15 @@
 "use client";
 
 import React from "react";
-import Head from "next/head";
 import Link from "next/link";
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
+import {
+  ContextMenu,
+  ContextMenuContent,
+  ContextMenuItem,
+  ContextMenuTrigger,
+} from "@/components/ui/context-menu";
 
 export default function HomePage() {
   const [message, setMessage] = React.useState("No message found");
@@ -16,32 +21,37 @@ export default function HomePage() {
   }, []);
 
   return (
-    <React.Fragment>
-      <Head>
-        <title>Home - Nextron (basic-lang-javascript)</title>
-      </Head>
-      <div>
-        <p className="font-bold">
-          ⚡ Electron + Next.js ⚡ - <Link href="/next">Go to next page</Link>
-        </p>
-        <Image
-          src="/images/logo.png"
-          alt="Logo image"
-          width={256}
-          height={256}
-        />
-      </div>
-      <div>
-        <button
-          onClick={() => {
-            window.ipc.send("message", "Hello");
-          }}
-        >
-          Test IPC
-        </button>
-        <Button>test</Button>
-        <p>{message}</p>
-      </div>
-    </React.Fragment>
+    <ContextMenu>
+      <ContextMenuTrigger>
+        <div>
+          <p className="font-bold">
+            ⚡ Electron + Next.js ⚡ - <Link href="/next">Go to next page</Link>
+          </p>
+          <Image
+            src="/images/logo.png"
+            alt="Logo image"
+            width={256}
+            height={256}
+          />
+        </div>
+        <div>
+          <button
+            onClick={() => {
+              window.ipc.send("message", "Hello");
+            }}
+          >
+            Test IPC
+          </button>
+          <Button>test</Button>
+          <p>{message}</p>
+        </div>
+      </ContextMenuTrigger>
+      <ContextMenuContent>
+        <ContextMenuItem>Profile</ContextMenuItem>
+        <ContextMenuItem>Billing</ContextMenuItem>
+        <ContextMenuItem>Team</ContextMenuItem>
+        <ContextMenuItem>Subscription</ContextMenuItem>
+      </ContextMenuContent>
+    </ContextMenu>
   );
 }
